ec0226084f9ba737c3bf81af6d876ac60818b981
Sumit <adeppasumit4@gmail.com>
2026-05-20 19:03:19 +0530

    Time: 1 ms (97.98%), Space: 42.3 MB (94.21%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -17,3 +17,30 @@ class Solution {
         return ans;
     }
 }
+
+/*
+function mySqrt(x):
+
+    if x == 0:
+        return 0
+
+    low = 1
+    high = x
+    ans = 0
+
+    while low <= high:
+
+        mid = low + (high - low) / 2
+
+        if mid * mid == x:
+            return mid
+
+        else if mid * mid < x:
+            ans = mid
+            low = mid + 1
+
+        else:
+            high = mid - 1
+
+    return ans
+*/
