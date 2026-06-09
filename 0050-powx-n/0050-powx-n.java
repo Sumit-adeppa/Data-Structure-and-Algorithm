29830e3762a7bac2a740f97aa6c91be932ada3de
Sumit <adeppasumit4@gmail.com>
2026-06-09 12:49:59 +0530

    Time: 0 ms (100%), Space: 47.9 MB (55.85%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,21 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x, n);
+        long N = n; // Handle Integer.MIN_VALUE
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        return power(x, N);
+    }
+
+    private double power(double x, long n) {
+        if (n == 0) {
+            return 1.0;
+        }
+        double half = power(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        }
+        return x * half * half;
     }
 }
