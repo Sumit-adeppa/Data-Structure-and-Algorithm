e7433073f27054da1935b0c2b1146ac1ecf06964
Sumit <adeppasumit4@gmail.com>
2026-07-15 23:07:48 +0530

    Time: 0 ms (100%), Space: 43.8 MB (46.76%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,33 +1,41 @@
 class Solution {
+
     public int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
+        return binarySearch(nums, target, 0, nums.length - 1);
+    }
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+    private int binarySearch(int[] nums, int target, int left, int right) {
 
-            if (nums[mid] == target) {
-                return mid;
-            }
+        // Base case
+        if (left > right) {
+            return -1;
+        }
 
-            // Left half is sorted
-            if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            }
-            // Right half is sorted
-            else {
-                if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
+        int mid = left + (right - left) / 2;
+
+        // Target found
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        // Left half is sorted
+        if (nums[left] <= nums[mid]) {
+
+            if (nums[left] <= target && target < nums[mid]) {
+                return binarySearch(nums, target, left, mid - 1);
+            } else {
+                return binarySearch(nums, target, mid + 1, right);
             }
         }
 
-        return -1;
+        // Right half is sorted
+        else {
+
+            if (nums[mid] < target && target <= nums[right]) {
+                return binarySearch(nums, target, mid + 1, right);
+            } else {
+                return binarySearch(nums, target, left, mid - 1);
+            }
+        }
     }
 }
