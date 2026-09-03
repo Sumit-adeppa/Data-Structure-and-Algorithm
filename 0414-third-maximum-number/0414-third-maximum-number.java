a96d55ef963df62914382a1c25b8e0563eb89833
Sumit <adeppasumit4@gmail.com>
2026-09-03 20:37:30 +0530

    Time: 1 ms (98.91%), Space: 44.9 MB (55.31%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,15 +1,22 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        TreeSet<Integer> set = new TreeSet<>();
+        long max1 = Long.MIN_VALUE;
+        long max2 = Long.MIN_VALUE;
+        long max3 = Long.MIN_VALUE;
+
         for (int num : nums) {
-            set.add(num);
-            if (set.size() > 3) {
-                set.pollFirst();
+            if (num == max1 || num == max2 || num == max3) continue;
+            if (num > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max3 = max2;
+                max2 = num;
+            } else if (num > max3) {
+                max3 = num;
             }
         }
-        if (set.size() == 3) {
-            return set.first();
-        }
-        return set.last();
+        return max3 == Long.MIN_VALUE ? (int) max1 : (int) max3;
     }
 }
